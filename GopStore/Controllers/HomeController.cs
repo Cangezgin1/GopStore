8134c2ab8be723e1c5d8912acf1ca4a947555ac8
Muhammed Can GEZGİN <93928479+Cangezgin1@users.noreply.github.com>
2022-12-14 15:08:34 +0300

    Düzenlemeler

--- GopStore/Controllers/HomeController.cs
+++ GopStore/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DataAccsessLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,9 +10,12 @@ namespace GopStore.Controllers
     public class HomeController : Controller
     {
 
-        public IActionResult Index()
+        Context c = new Context();
+
+        public IActionResult StudentList()   
         {
-            return View();
+            var values = c.Users.ToList();
+            return View(values);
         }
 
     }
